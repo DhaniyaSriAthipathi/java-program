0b2e07a5aa67f607174c4a6850fa51fe55e28d4c
DhaniyaSriAthipathi <athipathidhaniya@gmail.com>
2026-03-02 20:54:15 +0530

    Add files via upload

---
+++ Grocery.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Grocery {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int sale=sc.nextInt();
+        int discount=sc.nextInt();
+        int gst=sc.nextInt();
+        int finalRev=(sale-(sale*discount/100))+((sale-(sale*discount/100))*gst/100);
+        System.out.println(finalRev);
+
+    }
+    
+}
