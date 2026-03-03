85286c07e0b7688be2c94853dfdb976e988581fe
DhaniyaSriAthipathi <athipathidhaniya@gmail.com>
2026-03-03 20:21:24 +0530

    Add files via upload

---
+++ Mobile1.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class Mobile1 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        double cost=sc.nextDouble();
+        double gb=sc.nextDouble();
+        double rate=sc.nextDouble();
+        double tax=sc.nextDouble();
+        double extra=gb*rate;
+        double total=cost+gb;
+        double finalBill=total+(total*tax/100);
+        System.err.println(finalBill);
+
+
+    }
+    
+}
