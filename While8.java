498b034fc2a3bd4b1dfd29c3ea8d9f2d5319d5ff
DhaniyaSriAthipathi <athipathidhaniya@gmail.com>
2026-03-15 14:44:04 +0530

    Add files via upload

---
+++ While8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class While8 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int digit=0;
+        int sum=0;
+        while(n>0)
+        {
+           digit=n%10;
+            sum=sum+digit;
+             n=n/10;
+        }
+        System.out.println("sum : "+sum);
+    }
+}
