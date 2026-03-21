aea307e576ee9b13a886f8f3dfb39bc2d195ca0b
DhaniyaSriAthipathi <athipathidhaniya@gmail.com>
2026-03-21 20:39:57 +0530

    Add files via upload

---
+++ Nested11.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Nested11 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int row=sc.nextInt();
+        for(int i=0;i<=row;i++)
+        {
+            int value=1;
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(value + " ");
+                value=value*(i-j)/(j+1);
+            }
+            System.out.println();
+        }
+    }
+}
